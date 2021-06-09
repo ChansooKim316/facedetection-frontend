d2ebbbc730d0ce49b6b727685a257fe580fb4bda
ChansooKim316 <58630853+ChansooKim316@users.noreply.github.com>
2021-06-09 18:28:08 +0900

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,9 +10,10 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			</p>
 			<div className='center'>   {/* f4 = size of 4 , pa2 = padding of 2 , w-70 = width of 70% */}
 				<div className='form center pa4 br3 shadow-5'>										{/* ㄴ> 70% of total width of the page */}
-					<input className='f4 pa2 w-70' type='tex' onChange={onInputChange}
+					<input className='f4 pa2 w-70' type='text' onChange={onInputChange}
 								 placeholder="Paste image URL here." 
 								 id="imageInput" />    {/*******  8.24 midified : added id="imageInput"  ******/}
+								 value="https://static01.nyt.com/images/2020/11/19/us/artificial-intelligence-fake-people-faces-promo-1605818328743/artificial-intelligence-fake-people-faces-promo-1605818328743-superJumbo-v2.jpg"
 					<button 
 						id="detectBtn"  	/***** 8.24 midified : added id="detectBtn" *****/
 						/* grow = grow when you hoover , link = there will be a link. bg-light = background light */
